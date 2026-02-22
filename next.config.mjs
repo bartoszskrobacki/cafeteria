9f4319c9f0f0103a22389859e081488de96cf833
Bartosz Skrobacki <bartosz.skrobacki@merce.com>
2026-02-22 16:11:32 +0100

    More fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
 
-// next-intl plugin imports @swc/core which has no FreeBSD native binary.
-// The plugin is only needed during `next build` (sets webpack alias for next-intl/config).
-// During `next start`, the alias is already baked into the compiled output.
-const isStart = process.argv.some((arg) => arg === 'start');
-
-let finalConfig = nextConfig;
-if (!isStart) {
-  const { default: createNextIntlPlugin } = await import('next-intl/plugin');
-  const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
-  finalConfig = withNextIntl(nextConfig);
-}
-
-export default finalConfig;
+export default withNextIntl(nextConfig);
